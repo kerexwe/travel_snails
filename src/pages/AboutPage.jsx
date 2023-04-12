96641f7964d11cb91a94572708be30cb614cba3d
Ani25ani <anjarsuper51@gmail.com>
2023-04-12 17:34:48 +0600

    fix:imported components

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer';
 import About from '../components/About/About';
+import Newsletter from '../components/Newsletter/Newsletter';
 
 function AboutPage(props) {
     return (
         <div>
            <Header/> 
            <About/>
-           <Newsletter/>
+           <Newsletter />
            <Footer/>
         </div>
     );
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 import ContactComponents from '../components/ContactComponents/ContactComponents';
+import Newsletter from '../components/Newsletter/Newsletter';
 
 function ContactPage(props) {
     return (
         <div>
             <Header/> 
             <ContactComponents/>
-            <Newsletter/>
+            <Newsletter />
            <Footer/>           
         </div>
     );
