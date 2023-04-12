70105c235b123131a54de1bfbd23da538630dc6c
Ani25ani <anjarsuper51@gmail.com>
2023-04-12 18:15:28 +0600

    feat/Our-Clients

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,12 +3,14 @@ import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer';
 import About from '../components/About/About';
 import Newsletter from '../components/Newsletter/Newsletter';
+import OurClients from '../components/OurClients/OurClients';
 
 function AboutPage(props) {
     return (
         <div>
            <Header/> 
            <About/>
+           <OurClients/>
            <Newsletter />
            <Footer/>
         </div>
