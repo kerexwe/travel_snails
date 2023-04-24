0b22f26ad77dac78ce51b5ab6c0f111201d1b504
Joldosh123456 <usenakunov98@gmail.com>
2023-04-24 20:11:23 +0300

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -53,104 +53,108 @@ function Header(props) {
 
   return (
     <header className={HeaderStyles}>
-      <div className={s.wrapper}>
-        {/* <a href="" id={s.logoM}>
-          <img src={logo} alt="" id={s.logo} style={{ display: isDarkMode ? "none" : "block" }} />
-          <img src={blacklogo} alt="" style={{ display: isDarkMode ? "block" : "none" }} />
-        </a> */}
-        <div className={s.left}>
-          <a href="" id={s.logoD}>
-            <img
-              src={logo}
-              alt=""
-              id={s.logo}
-              style={{ display: "light" ? "none" : "block" }}
-            />
-            <img
-              src={blacklogo}
-              alt=""
-              style={{ display: "dark" ? "block" : "none" }}
-            />
-          </a>
-          <div>
-            {" "}
-            <a href="/">{t("header.home")}</a>
-          </div>
-          <a href="">{t("header.components")}</a>
-          <li>
-            <div className={s.page}>
-              <img src={dot} alt="" />
-              <a href="">{t("header.pages")}</a>
-              <img src={vector} alt="" />
-            </div>
-            <ul>
-              <li>
-                <a href="/">{t("header.home")}</a>
-              </li>
-              <li>
-                <a href="/list">{t("header.tours")}</a>
-              </li>
-              <li>
-                <a href="/details">{t("header.details")}</a>
-              </li>
-              <li>
-                <a href="/checkout">{t("header.checkout")}</a>
-              </li>
-              <li>
-                <a href="/completed">{t("header.complete")}</a>
-              </li>
-              <li>
-                <a href="/blog">{t("header.blog")}</a>
-              </li>
-              <li>
-                <a href="/blog-details">{t("header.blogDet")}</a>
-              </li>
-              <li>
-                <a href="/about">{t("header.about")}</a>
-              </li>
-              <li>
-                <a href="/contact">{t("header.contact")}</a>
-              </li>
-            </ul>
-          </li>
-          <a href="">{t("header.docum")}</a>
-        </div>
-        <img src={burger} alt="" id={s.burger} />
-        <div className={s.right}>
-          <label class="toggle" for="myToggle">
-            <input
-              class="toggle__input"
-              name=""
-              type="checkbox"
-              id="myToggle"
-              onClick={switchTheme}
-            />
-            <div class="toggle__fill"></div>
-          </label>
-
-          <img src={search} alt="" />
-          <li>
-            <img src={browser} alt="" />
-            <ul>
-              <li>
-                <button onClick={() => changeLanguage("ru")}>RU</button>
-              </li>
-              <li>
-                <button onClick={() => changeLanguage("en")}>EN</button>
-              </li>
-            </ul>
-          </li>
-          <p>|</p>
-          <a href="">{t("header.login")}</a>
-          <a href="">
-            <button>
-              <a href="">{t("header.join")}</a>
-            </button>
-          </a>
-        </div>
-      </div>
+      
     </header>
   );
 }
 
 export default Header;
+
+
+// <div className={s.wrapper}>
+//         {/* <a href="" id={s.logoM}>
+//           <img src={logo} alt="" id={s.logo} style={{ display: isDarkMode ? "none" : "block" }} />
+//           <img src={blacklogo} alt="" style={{ display: isDarkMode ? "block" : "none" }} />
+//         </a> */}
+//         <div className={s.left}>
+//           <a href="" id={s.logoD}>
+//             <img
+//               src={logo}
+//               alt=""
+//               id={s.logo}
+//               style={{ display: "light" ? "none" : "block" }}
+//             />
+//             <img
+//               src={blacklogo}
+//               alt=""
+//               style={{ display: "dark" ? "block" : "none" }}
+//             />
+//           </a>
+          
+//           <div>
+//             {" "}
+//             <a href="/">{t("header.home")}</a>
+//           </div>
+//           <a href="">{t("header.components")}</a>
+//           <li>
+//             <div className={s.page}>
+//               <img src={dot} alt="" />
+//               <a href="">{t("header.pages")}</a>
+//               <img src={vector} alt="" />
+//             </div>
+//             <ul>
+//               <li>
+//                 <a href="/">{t("header.home")}</a>
+//               </li>
+//               <li>
+//                 <a href="/list">{t("header.tours")}</a>
+//               </li>
+//               <li>
+//                 <a href="/details">{t("header.details")}</a>
+//               </li>
+//               <li>
+//                 <a href="/checkout">{t("header.checkout")}</a>
+//               </li>
+//               <li>
+//                 <a href="/completed">{t("header.complete")}</a>
+//               </li>
+//               <li>
+//                 <a href="/blog">{t("header.blog")}</a>
+//               </li>
+//               <li>
+//                 <a href="/blog-details">{t("header.blogDet")}</a>
+//               </li>
+//               <li>
+//                 <a href="/about">{t("header.about")}</a>
+//               </li>
+//               <li>
+//                 <a href="/contact">{t("header.contact")}</a>
+//               </li>
+//             </ul>
+//           </li>
+//           <a href="">{t("header.docum")}</a>
+//         </div>
+//         <img src={burger} alt="" id={s.burger} />
+//         <div className={s.right}>
+//           <label class="toggle" for="myToggle">
+//             <input
+//               class="toggle__input"
+//               name=""
+//               type="checkbox"
+//               id="myToggle"
+//               onClick={switchTheme}
+//             />
+//             <div class="toggle__fill"></div>
+//           </label>
+
+//           <img src={search} alt="" />
+//           <li>
+//             <img src={browser} alt="" />
+//             <ul>
+//               <li>
+//                 <button onClick={() => changeLanguage("ru")}>RU</button>
+//               </li>
+//               <li>
+//                 <button onClick={() => changeLanguage("en")}>EN</button>
+//               </li>
+//             </ul>
+//           </li>
+//           <p>|</p>
+//           <a href="">{t("header.login")}</a>
+//           <a href="">
+//             <button>
+//               <a href="">{t("header.join")}</a>
+//             </button>
+//           </a>
+//         </div>
+//       </div>
